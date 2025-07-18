f976b52c3a091893f9f79af4e7face6a00221205
Pawandeep Suryavanshi <suryavanshipawan21@gmail.com>
2025-07-18 18:11:51 +0530

    fix ek krne wala issue

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,16 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import connectDB from './config/db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import authRouter from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import hostRoutes from './routes/hotel.routes.js';
 import bookingRoutes from './routes/booking.routes.js';
 import paymentRoutes from './routes/payment.routes.js';
-import serviceRoutes from './routes/service.routes.js';     // ✅ NEW
-import experienceRoutes from './routes/experience.routes.js'; // ✅ NEW
+import serviceRoutes from './routes/service.routes.js';     
+import experienceRoutes from './routes/experience.routes.js';
 import reviewRoutes from './routes/review.routes.js';
 import subscriptionRoutes from "./routes/subscription.routes.js";
 import superAdminRoutes from "./routes/superadmin.routes.js";
@@ -35,6 +37,15 @@ app.use("/api/subscription", subscriptionRoutes);
 
 
 app.use("/api/super-admin", superAdminRoutes);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendDistPath = path.join(__dirname, '..', 'frontend', 'dist');
+app.use(express.static(frontendDistPath));
+
+app.all('*', (req, res) => {
+  res.sendFile(path.join(frontendDistPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
